bf0b08ffe3625c019716b87c9fa9c37898eac071
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-05-05 17:00:15 +0530

    First Commit

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+// Button.js
+import React from 'react';
+const button = (props) => {
+  const classes = ['btn'];
+  if (typeof props !== 'undefined' && typeof props.type !== 'undefined')
+    classes.push('btn--' + props.type);
+  return (
+    <button className={classes.join(' ')} onClick={props.onButtonPress}>
+      {props.children}
+    </button>
+  );
+}
+export default button;
